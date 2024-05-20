9c13fd19db7f1b166cfb7e68e559aad95c549614
kittycat345 <ilyapavl0v531@gmail.com>
2024-05-20 11:55:41 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+count_home_work=12
+time_is_spended_for_hw=1.5
+name_of_course="Python"
+time_for_one_task=time_is_spended_for_hw/count_home_work
+print("Курс: " +name_of_course+ ", всего задач:" + str(count_home_work)+", затрачено часов: "+str(time_is_spended_for_hw)+", среднее время выполнения "+str(time_for_one_task)+" часа.")
